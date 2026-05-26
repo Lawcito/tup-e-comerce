cf7c9ce238a4ff052a4d94b01c865763eab71c6b
Lawcito <emiraleconegrete@gmail.com>
2026-05-26 01:00:56 -0300

    first commit

--- src/app/services/items.ts
+++ src/app/services/items.ts
@@ -1,58 +1,27 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
 import { Observable, of, tap } from 'rxjs';
 import { Item } from '../models/item';
-
-interface ItemsCache {
-  data: Item[];
-  expiresAt: number;
-}
+import { ItemsApiService } from './items-api.service';
+import { ItemsStorageService } from './items-storage.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ItemsService {
-  private readonly apiUrl = 'http://makeup-api.herokuapp.com/api/v1/products.json';
-  private readonly cacheKey = 'items-cache';
-  private readonly cacheDuration = 5 * 60 * 1000;
-
-  constructor(private http: HttpClient) {}
+  constructor(
+    private itemsApiService: ItemsApiService,
+    private itemsStorageService: ItemsStorageService
+  ) {}
 
   getItems(): Observable<Item[]> {
-    const cachedItems = this.getCachedItems();
+    const cachedItems = this.itemsStorageService.getCachedItems();
 
     if (cachedItems) {
       return of(cachedItems);
     }
 
-    return this.http.get<Item[]>(this.apiUrl).pipe(
-      tap((items) => this.saveItemsInCache(items))
+    return this.itemsApiService.fetchItems().pipe(
+      tap((items) => this.itemsStorageService.saveItemsInCache(items))
     );
-  }
-
-  private getCachedItems(): Item[] | null {
-    const cachedValue = localStorage.getItem(this.cacheKey);
-
-    if (!cachedValue) {
-      return null;
-    }
-
-    const cache: ItemsCache = JSON.parse(cachedValue);
-
-    if (Date.now() > cache.expiresAt) {
-      localStorage.removeItem(this.cacheKey);
-      return null;
-    }
-
-    return cache.data;
-  }
-
-  private saveItemsInCache(items: Item[]): void {
-    const cache: ItemsCache = {
-      data: items,
-      expiresAt: Date.now() + this.cacheDuration
-    };
-
-    localStorage.setItem(this.cacheKey, JSON.stringify(cache));
   }
 }
