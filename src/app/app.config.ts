77a42c0a861476e5e0237b5439483c977cf7b623
Norali Gorosito <norali84g@gmail.com>
2026-07-20 20:04:31 -0300

    fix: eliminar duplicados de service worker y manifest en PR #42

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -15,9 +15,5 @@ export const appConfig: ApplicationConfig = {
       enabled: !isDevMode(),
       registrationStrategy: 'registerWhenStable:30000',
     }),
-    provideServiceWorker('ngsw-worker.js', {
-      enabled: !isDevMode(),
-      registrationStrategy: 'registerWhenStable:30000',
-    }),
   ],
 };
